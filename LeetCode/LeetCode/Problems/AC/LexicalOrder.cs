a37850d5734dac988fa97e85229fd6a89b18b6e5
zhanghongfei <hongfei233@gmail.com>
2019-11-09 22:11:17 +0800

    add ac file

---
+++ LeetCode/LeetCode/Problems/AC/LexicalOrder.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Problems
+{
+    /// <summary>
+    /// 386. Lexicographical Numbers
+    /// </summary>
+    public partial class Problem
+    {
+        public IList<int> LexicalOrder(int n)
+        {
+            List<int> result = new List<int>();
+            for (int i = 1; i < 10; i++)
+            {
+                F(i, n, ref result);
+            }
+            return result;
+        }
+
+        private void F(int value, int maxValue, ref List<int> temp)
+        {
+            if (value > maxValue) return;
+            temp.Add(value);
+            value *= 10;
+            for (int i = 0; i < 10; i++)
+            {
+                F(value + i, maxValue, ref temp);
+            }
+        }
+    }
+}
--- LeetCode/LeetCode/Problems/Problem.cs
+++ LeetCode/LeetCode/Problems/Problem.cs
@@ -2,27 +2,11 @@
 
 namespace LeetCode.Problems
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public partial class Problem
     {
-        public IList<int> LexicalOrder(int n)
-        {
-            List<int> result = new List<int>();
-            for (int i = 1; i < 10; i++)
-            {
-                F(i, n, ref result);
-            }
-            return result;
-        }
 
-        private void F(int value, int maxValue, ref List<int> temp)
-        {
-            if (value > maxValue) return;
-            temp.Add(value);
-            value *= 10;
-            for (int i = 0; i < 10; i++)
-            {
-                F(value + i, maxValue, ref temp);
-            }
-        }
     }
 }
