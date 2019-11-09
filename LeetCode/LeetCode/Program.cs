309e8e25c2dbd402881b67a60d81450d2d265a8f
zhanghongfei <hongfei233@gmail.com>
2019-11-09 22:02:34 +0800

    Init

---
+++ LeetCode/LeetCode/Program.cs
@@ -0,0 +1,20 @@
+﻿using LeetCode.Problems;
+using System;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Problem pm = new Problem();
+            var list = pm.LexicalOrder(13);
+            foreach(var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.Read();
+        }
+    }
+}
